58869cd2dd1780ebc747ef485e6cc271be5e010b
Devon Harris <devon_harris@glic.com>
2022-08-22 12:12:20 -0400

    changing print statement

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ import fs from "fs";
 
 let run_demo_action = async function () {
   try {
-    const input = core.getInput("directory-input");
+    //const input = core.getInput("directory-input");
 
+    const input = "/usr/src/app/"
     var fileList = [];
     fs.readdir(input, (err, files) => {
       console.log(files);
       files.forEach((file) => {
-        if (file.includes(lookup)) {
           fileList.push(file.toString());
-        }
       });
     });
 
-    core.info("From the directory mounted location " + input+" we discovered these files " + fileList);
+    core.info("we discovered these files " + fileList);
 
   } catch (error) {
     core.setFailed(error.message);
