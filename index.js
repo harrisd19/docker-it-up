2adcc7196ca7ceab71a65a573d244f5d63d8e70f
Devon Harris <devon_harris@glic.com>
2022-08-25 14:14:19 -0400

    adding mulitple inputs

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ let run_demo_action = async function () {
   try {
     //const input = core.getInput("directory-input");
 
-    const input = "/usr/src/app/"
+    const input = ["/usr/src/app/", ""]
+    input.forEach((n) => {
     var fileList = [];
-    fs.readdir(input, (err, files) => {
+    fs.readdir(n, (err, files) => {
       console.log(files);
       files.forEach((file) => {
           fileList.push(file.toString());
       });
     });
 
-    core.info("we discovered these files " + fileList);
-
+    core.info("we discovered these files " + n + fileList);
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
